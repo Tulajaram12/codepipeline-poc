7f19d3bb4051c8a7834963bc5fa329650719c18d
Tulajaram12 <96332761+Tulajaram12@users.noreply.github.com>
2026-08-12 22:54:32 +0530

    Update home message and change port to 8080

--- app.py
+++ app.py
@@ -4,12 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Pipeline POC is working!"
+    return "Hello from AWS CodePipeline!"
 
 @app.route("/health")
 def health():
     return {"status": "UP"}
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8080)
